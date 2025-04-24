96bb80a3e193c92b5ff54682c228c8cfe3d47944
TheoDrHashiriya <theotrisha@gmail.com>
2025-04-23 22:06:13 -0700

    added Student struct!

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 
+struct Student {
+  std::string name;
+  std::string studentID;
+  std::string course;
+  int yearLevel;
+  float finalGrade;
+};
+
 int main() { std::cout << "Hewwo uwu\n"; }
