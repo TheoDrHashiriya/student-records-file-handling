a5748c5657cbdcfb4a0d09d867d24a3d08c5ec0f
unknown <alelybitun.school@gmail.com>
2025-04-24 09:11:17 +0800

    Adding createNewStudent in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <ios>
 #include <iostream>
+#include <ios>
+#include <fstream>
 #include <limits>
+
 
 const int MAX_STUDENTS = 1000;
 
@@ -12,34 +14,97 @@ struct Student {
   float finalGrade;
 };
 
-int main() {
-  int option;
-  std::cout << "1. Add a student record\n"
-               "2. View all records\n"
-               "3. Search a record by Student ID\n"
-               "4. Update a record\n"
-               "5. Delete a record\n"
-               "6. Exit\n"
-               "Enter option: ";
+void createNewStudent(Student stu[], int count){
+  std::ofstream outFile("data.txt", std::ios::app);
+  Student newStudent;
 
-  bool isValid = false;
+  if (outFile.is_open()){
+    std::cout << "ADD NEW INFO" << std::endl;
+    
+    std::cout << "Enter an ID: ";
+    std::cin >> newStudent.studentID;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-  // Input validation for option
-  while (!isValid) {
-    if (!(std::cin >> option)) {
-      std::cout << "Option should be a number! Try again: ";
+    std::cout << "Enter the Name: ";
+    std::getline(std::cin, newStudent.name); 
+
+    std::cout << "Enter the Course: ";
+    std::getline(std::cin,newStudent.course);
+
+    std::cout << "Enter Year Level: ";
+    while (!(std::cin >> newStudent.yearLevel)) {
+      std::cout << "Invalid input. Enter a number for Year Level: ";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      continue;
+    }
+
+    std::cout << "Enter the Final Grade: ";
+    std::cin >> newStudent.finalGrade;
+
+    outFile << newStudent.name << " " << newStudent.studentID << " " 
+            << newStudent.course << " " << newStudent.yearLevel << " " 
+            << newStudent.finalGrade << std::endl;
+    outFile.close();
+    
+    stu[count++] = newStudent;
+    std::cout << "\n Added Succefully!!" << std::endl;
+  } else {
+    std::cerr << "Failed to open the file for writing" << std::endl;
+  }
+
+
+}
+
+int main() {
+  int option;
+  int count = 0;
+  Student studentRecord[MAX_STUDENTS];
+
+  do{
+    
+    std::cout << "1. Add a student record\n"
+    "2. View all records\n"
+    "3. Search a record by Student ID\n"
+    "4. Update a record\n"
+    "5. Delete a record\n"
+    "6. Exit\n"
+    "Enter option: ";
+
+    bool isValid = false;
+
+    // Input validation for option
+    while (!isValid) {
+    if (!(std::cin >> option)) {
+    std::cout << "Option should be a number! Try again: ";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    continue;
     }
 
     if (option < 1 || option > 6) {
-      std::cout << "Option should be between 1 and 6! Try again: ";
-      continue;
+    std::cout << "Option should be between 1 and 6! Try again: ";
+    continue;
     } else {
-      isValid = true;
+    isValid = true;
     }
-  }
+    }
 
-  // Main logic stuff
+    // Main logic stuff
+    switch (option) {
+      case 1:
+        createNewStudent(studentRecord,count);
+        break;
+      case 2:
+        
+        break;
+
+    default:
+      break;
+    } 
+
+  } while (option != 0);
+  
+
+
+
 }
