72a9efa2fb56aada756ae5a97adb967d31202450
TheoDrHashiriya <theotrisha@gmail.com>
2025-04-23 22:11:38 -0700

    added initial menu codelines

--- main.cpp
+++ main.cpp
@@ -8,4 +8,13 @@ struct Student {
   float finalGrade;
 };
 
-int main() { std::cout << "Hewwo uwu\n"; }
+int main() {
+  int option;
+  std::cout << "1. Add a student record\n"
+               "2. View all records\n"
+               "3. Search a record by Student ID\n"
+               "4. Update a record\n"
+               "5. Exit\n"
+               "Enter option: ";
+  std::cin >> option;
+}
