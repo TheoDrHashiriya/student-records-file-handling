22ce75102c171d3f2015ae42d2ba5111620bdcf1
TheoDrHashiriya <theotrisha@gmail.com>
2025-04-23 22:30:16 -0700

    added input validation for option input and max students constant

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <ios>
 #include <iostream>
+#include <limits>
+
+const int MAX_STUDENTS = 1000;
 
 struct Student {
   std::string name;
@@ -16,5 +20,25 @@ int main() {
                "4. Update a record\n"
                "5. Exit\n"
                "Enter option: ";
-  std::cin >> option;
+
+  bool isValid = false;
+
+  // Input validation for option
+  while (!isValid) {
+    if (!(std::cin >> option)) {
+      std::cout << "Option should be a number! Try again: ";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+
+    if (option < 1 || option > 5) {
+      std::cout << "Option should be between 1 and 5! Try again: ";
+      continue;
+    } else {
+      isValid = true;
+    }
+  }
+
+  // Main logic stuff
 }
